91010046b94777f9720801ae63f1c8423dea85f9
genesisjk <genesisjk@gmail.com>
2023-03-16 15:20:33 +0900

    first Commit

---
+++ src/main/java/com/nhncloud/ncrtest/NcrtestApplication.java
@@ -0,0 +1,14 @@
+package com.nhncloud.ncrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcrtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcrtestApplication.class, args);
+
+		System.out.println("Hello NHN Cloud!!");
+	}
+}
---
+++ src/test/java/com/nhncloud/ncrtest/NcrtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhncloud.ncrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcrtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
