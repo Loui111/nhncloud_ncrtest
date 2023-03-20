7f35b51b956f5c07de1add6093941c020bd4d295
genesisjk <genesisjk@gmail.com>
2023-03-20 22:44:01 +0900

    컨트롤러 추가

---
+++ src/main/java/com/nhncloud/ncrtest/Controller.java
@@ -0,0 +1,13 @@
+package com.nhncloud.ncrtest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @RequestMapping(value = "/hello")
+  public String hello() {
+    return "Hello NHN Cloud!!";
+  }
+}
